21f7cc10af0712c1876e0f80ba5c984e76db31e3
Yishan Zhao <yishanzh@umd.edu>
2021-09-08 23:58:39 -0400

    I have a new idea

---
+++ ToDue2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDue2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public Task CancelShowDesktop(Window window)
+		{
+			return Task.Factory.StartNew(() =>
+			{
+				this.Dispatcher.Invoke(() =>
+				{
+					Thread.Sleep(100);
+					window.WindowState = WindowState.Normal;
+					window.Topmost = true;
+					window.Topmost = false;
+				});
+			});
+		}
+
+		private async void Window_StateChanged(object sender, EventArgs e)
+		{
+			await CancelShowDesktop(this);
+		}
+	}
+}
